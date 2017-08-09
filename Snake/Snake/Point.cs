e96dfc34b39b075e8920c5eb71651499aafd7a87
asbelous <arcaim@list.ru>
2017-08-09 11:38:56 +0700

    Создан класс Змейка

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -19,6 +19,32 @@ namespace Snake
             sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
+        }
 
         public void Draw()
         {
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -28,7 +28,8 @@ namespace Snake
             //Отрисовка точки
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
         }
